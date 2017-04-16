4da90281b3f52b8ce37edef39da258a5007cc6a2
XboxYan’Mac <xboxyan@XboxYanMacdeMac-Pro.local>
2017-04-16 23:43:24 +0800

    tab

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,193 @@ import React, { PureComponent } from 'react';
 import {
   StyleSheet,
   Text,
+  ScrollView,
   View,
 } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 
+
+class HomeView extends React.PureComponent {
+    componentDidMount(){
+        alert(1)
+    }
+    render(){
+        return(
+            <ScrollView style={{flex:1}}>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+            </ScrollView>
+        )
+    }
+}
+
+class ProfileView extends React.PureComponent {
+    componentDidMount(){
+        alert(2)
+    }
+    render(){
+        return(
+            <ScrollView style={{flex:1}}>
+                <View><Text>2222222</Text></View>
+                <View><Text>2222222</Text></View>
+                <View><Text>2222222</Text></View>
+                <View><Text>2222222</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+                <View><Text>111111</Text></View>
+            </ScrollView>
+        )
+    }
+}
 
 export default class Home extends React.PureComponent {
+    state={
+        selectedTab:'home'
+    }
     render(){
         return (
-            <Text>这是首页！</Text>
+            <TabNavigator>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'home'}
+                    title="Home"
+                    //renderIcon={() => <Image source={...} />}
+                    //renderSelectedIcon={() => <Image source={...} />}
+                    onPress={() => this.setState({ selectedTab: 'home' })}>
+                    <HomeView/>
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    selected={this.state.selectedTab === 'profile'}
+                    title="Profile"
+                    //renderIcon={() => <Image source={...} />}
+                    //renderSelectedIcon={() => <Image source={...} />}
+                    //renderBadge={() => <CustomBadgeView />}
+                    onPress={() => this.setState({ selectedTab: 'profile' })}>
+                    <ProfileView/>
+                </TabNavigator.Item>
+            </TabNavigator>
         )
     }
 }
